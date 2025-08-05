50d7377b71c6a1160aca1eb86ee29083facce6e5
Jose_Apaza <jose.apazasev@gmail.com>
2025-08-05 12:37:57 -0500

    added list component for task

--- src/app/tasks/task-list/task-list.ts
+++ src/app/tasks/task-list/task-list.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Task } from '../../models/task.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './task-list.html',
   styleUrl: './task-list.scss'
 })
 export class TaskList {
+
+  /* Objects */
+  tasks: Task[] = [
+    {
+      id: uuidv4(),
+      title: 'Diseñar interfaz de login',
+      description: 'Crear UI con Angular Material y validaciones.',
+      status: 'pendiente',
+      createdAt: new Date()
+    },
+    {
+      id: uuidv4(),
+      title: 'Conectar API de tareas',
+      description: 'Usar servicio en Angular para obtener datos.',
+      status: 'en-progreso',
+      createdAt: new Date()
+    },
+    {
+      id: uuidv4(),
+      title: 'Pruebas de funcionalidad',
+      description: 'Asegurar que todo funcione correctamente.',
+      status: 'completada',
+      createdAt: new Date()
+    }
+  ];
+
 
 }
